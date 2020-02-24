0c226fd3f48e0356da86c968babbf6d142d32d8f
Isaac Martinez <IsaacR454@gmail.com>
2020-02-24 17:53:54 -0600

    Initial Commit

---
+++ Assignment_3/Program.fs
@@ -0,0 +1,26 @@
+﻿let rec printMatrix list =
+    match list with
+    | head::tail -> printfn "%A" head; printMatrix tail 
+    | [] -> printfn ""
+    
+let swaps
+    
+let rec move (list: int List) =
+    match list with
+    | head::tail -> if head <> 0 while (list) do
+        
+    
+    
+
+[<EntryPoint>]
+let main argv =
+    let row1 = [1;1;1;1;1]
+    let row2 = [0;0;0;0;0]
+    let row3 = [0;0;3;0;0]
+    let row4 = [0;0;0;0;0]
+    let row5 = [2;2;2;2;2]
+    
+    let board = [row1;row2;row3;row4;row5]
+    
+    printMatrix board
+    0 // return an integer exit code
